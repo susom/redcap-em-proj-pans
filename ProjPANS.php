9ac75b4518b7b7c1a50c8e529fb1195c38b79ab5
Jae Lee <jael@stanford.edu>
2021-02-24 11:59:47 -0800

    Add redcap_module_link_check_display so that non design users can access links

--- ProjPANS.php
+++ ProjPANS.php
@@ -10,4 +10,12 @@ class ProjPANS extends \ExternalModules\AbstractExternalModule
 {
     use emLoggerTrait;
 
+    function redcap_module_link_check_display($project_id, $link){
+        if($this->linkCheckDisplayReturnValue !== null){
+            return $this->linkCheckDisplayReturnValue;
+        }
+
+        return parent::redcap_module_link_check_display($project_id, $link);
+    }
+
 }
