18d6fef7d3096cfcb0e4ac881330ca9dfdb3937c
Jae Lee <jael@stanford.edu>
2021-02-26 09:45:10 -0800

    One more attempt to allow none-design rights users see EM links

--- ProjPANS.php
+++ ProjPANS.php
@@ -11,11 +11,17 @@ class ProjPANS extends \ExternalModules\AbstractExternalModule
     use emLoggerTrait;
 
     function redcap_module_link_check_display($project_id, $link){
+
+        //returning link will bypass the user rights check that the user have Design Rights.
+        return $link;
+
+        /**
         if($this->linkCheckDisplayReturnValue !== null){
             return $this->linkCheckDisplayReturnValue;
         }
 
         return parent::redcap_module_link_check_display($project_id, $link);
+         */
     }
 
 }
